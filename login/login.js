dd1887adde93d5b345dff29c1f6671d5e11e5704
hyojoo <jhj080802@naver.com>
2024-06-13 20:01:45 +0900

    feat : 로그인 routing

--- login/login.js
+++ login/login.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
+    const loginForm = document.getElementById('login-form');
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const loginButton = document.querySelector('.login-button');
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
 
         if (loginButton.classList.contains('active')) {
-            // 로그인 로직을 여기에 추가
             alert('로그인 성공');
         } else {
             alert('아이디와 비밀번호를 6자 이상 입력해주세요');
