d377f6d25adbca27e78ed59b04cfbe3eb99a9c9c
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-06-19 13:22:22 +0900

    fix : if문

--- new_password/new_password.js
+++ new_password/new_password.js
@@ -10,23 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const profile = document.querySelector("#profile");
     let userId = "";
 
-    if (profile) {
         profile.addEventListener("click", function(){
             window.location.href = `/profile?id=${userId}`;
         });
-    }
 
-    if (logo) {
         logo.addEventListener('click', function(){
             window.location.href = "/";
         });
-    }
+    
 
     function btnColor() {
         const passwordValue = passwordInput.value;
         const newpasswordValue = newpasswordInput.value;
 
-        if (passwordValue !== "" && newpasswordValue !== "") {
+        if (passwordValue !== "" && newpasswordValue !== ""){
             nextBtn.style.color = "white";
             nextBtn.style.background = "var(--Main, #3269F6)";
         } else {
@@ -124,34 +121,27 @@ document.addEventListener("DOMContentLoaded", function () {
     function redirectToSignup() {
         window.location.href = "/signup";
     }
-
-    if (nextBtn) {
+ 
         nextBtn.addEventListener("click", changePassword);
-    }
+    
 
     const nextForm = document.getElementById("nextform");
-    if (nextForm) {
         nextForm.addEventListener("submit", changePassword);
-    }
+    
 
-    if (passwordInput) {
+
         passwordInput.addEventListener("input", btnColor);
         passwordInput.addEventListener("input", validatePassword);
         passwordInput.addEventListener("input", checkPassword);
-    }
+    
 
-    if (newpasswordInput) {
+
         newpasswordInput.addEventListener("input", btnColor);
         newpasswordInput.addEventListener("input", checkPassword);
-    }
-
-    if (loginBtn) {
+ 
         loginBtn.addEventListener("click", redirectToLogin);
-    }
 
-    if (signupBtn) {
         signupBtn.addEventListener("click", redirectToSignup);
-    }
 
     getUserId();
 });
