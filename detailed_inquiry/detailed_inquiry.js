1e3c6c428157647cf39fd42c0868c0c395b4b00c
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-06-20 19:11:59 +0900

    Revert "Merge branch 'develop' into feature3"
    This reverts commit 8929a9924d6f1345f4d13fd5567ab8cc214a824b, reversing
    changes made to 5b318c87111961e9528fc8510e7f04bb8fb2d203.

--- detailed_inquiry/detailed_inquiry.js
+++ detailed_inquiry/detailed_inquiry.js
@@ -1,10 +1,9 @@
-const hostURL =
-    "https://port-0-onboarding-server-f02w2almh8gdgs.sel5.cloudtype.app";
+const hostURL = "https://port-0-onboarding-server-f02w2almh8gdgs.sel5.cloudtype.app";
 const pageTitle = document.querySelector(".page-title");
 const article = document.querySelector(".article");
-const viewCount = document.querySelector("#view-count");
+const viewCount = document.querySelector("#view-count")
 const likes = document.querySelector("#heart-count");
-const commentList = document.getElementById("comment-list");
+const commentList = document.getElementById('comment-list');
 const details = document.querySelector("#details");
 const writing = document.querySelector("#writing");
 const myProfile = document.querySelector("#myProfile");
@@ -12,34 +11,28 @@ const heartBtn = document.querySelector("#heart-button");
 
 let isLogined = false;
 let user;
-const fetchUserUrl = `${hostURL}/api/user`;
-fetch(fetchUserUrl, { credentials: "include" }).then((response) => {
-    if (response.ok) {
-        response.json().then((userData) => {
+const fetchUserUrl = `http://${hostURL}/api/user/`;
+fetch(fetchUserUrl).then(response => {
+    if(response.ok){
+        response.json().then((userData)=>{
             user = userData;
             isLogined = true;
         });
-    } else {
-        response.text().then((text) => console.log(text));
     }
 });
 
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
-if (id === null) {
-    pageTitle.textContent = "게시글 정보가 없습니다.";
-    article.textContent = "정보 없음";
-}
+if(id === null) {pageTitle.textContent = "게시글 정보가 없습니다."; article.textContent="정보 없음";}
 
 console.log(id);
 
 let isPostLoading = true;
 let post = {};
-const fetchPostsUrl = `${hostURL}/api/post/${id}`;
-const fetchCommentsUrl = `${hostURL}/api/post/${id}/comments`;
-
+const fetchPostsUrl = `http://${hostURL}/api/post/${id}`;
+const fetchCommentsUrl = `http://${hostURL}/api/post/${id}/comments`;
 // fetch를 사용한 게시글 요청
-fetch(fetchPostsUrl, { credentials: "include" })
+fetch(fetchPostsUrl)
     .then((response) => {
         if (!response.ok) {
             throw new Error("Network response was not ok");
@@ -48,13 +41,11 @@ fetch(fetchPostsUrl, { credentials: "include" })
     })
     .then((_post) => {
         post = _post;
-        if (!isLogined || user.name !== post.author) {
+        if(!isLogined || user.id !== post.id){
             details.outerHTML = "";
             writing.outerHTML = "";
-        }
-        if (!isLogined) {
             myProfile.outerHTML = "";
-            heartBtn.setAttribute("disabled", "true");
+            heartBtn.setAttribute('disabled','true');
         }
         pageTitle.textContent = post.title;
         article.textContent = post.content;
@@ -64,10 +55,10 @@ fetch(fetchPostsUrl, { credentials: "include" })
         console.log("게시글:", post);
     })
     .catch((error) => {
-        console.error("게시글 요청 에러:", error, fetchPostsUrl);
+        console.error("게시글 요청 에러:", error,fetchPostsUrl);
     });
 
-fetch(fetchCommentsUrl, { credentials: "include" })
+fetch(fetchCommentsUrl)
     .then((response) => {
         if (!response.ok) {
             throw new Error("Network response was not ok");
@@ -75,8 +66,8 @@ fetch(fetchCommentsUrl, { credentials: "include" })
         return response.json();
     })
     .then((comments) => {
-        comments.forEach((comment) => {
-            const template = document.createElement("template");
+        comments.forEach((comment)=>{
+            const template = document.createElement('template');
             template.innerHTML = `<div class="comment">
                     <svg class="profile" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
                         <g clip-path="url(#clip0_2202_235)">
@@ -92,7 +83,9 @@ fetch(fetchCommentsUrl, { credentials: "include" })
                     <strong class="commentAuthor">${comment.author}</strong>
                     <p class="commentContent">${comment.content}</p>
                 </div>`;
-            commentList.appendChild(template.content.firstChild);
+            commentList.appendChild(
+                template.content.firstChild
+            )
         });
         console.log("게시글:", comments);
     })
@@ -130,15 +123,11 @@ editButton.addEventListener("click", function () {
         editButton.textContent = "수정";
         post.title = pageTitle.textContent;
         post.content = article.textContent;
-        fetch(fetchPostsUrl, {
-            method: "PATCH",
-            body: {
-                title: post.title,
-                content: post.content,
-            },
-            credentials: "include",
-        }).then((response) => {
-            if (!response.ok) alert("게시글 수정에 실패했습니다.");
+        fetch(fetchPostsUrl,{method:"PATCH",body:{
+            title:post.title,
+            content:post.content
+        }}).then(response=>{
+            if(!response.ok) alert("게시글 수정에 실패했습니다.");
         });
     } else {
         pageTitle.contentEditable = "true";
@@ -149,6 +138,7 @@ editButton.addEventListener("click", function () {
 });
 
 pageTitle.addEventListener("input", () => {
+
     if (pageTitle.textContent.length > 15) {
         pageTitle.textContent = pageTitle.textContent.substring(0, 15);
         placeCaretAtEnd(pageTitle);
@@ -164,12 +154,11 @@ article.addEventListener("input", () => {
 
 deleteButton.addEventListener("click", function () {
     if (confirm("정말로 삭제하시겠습니까?")) {
-        fetch(fetchPostsUrl, { method: "DELETE", credentials: "include" }).then(
-            (response) => {
-                if (!response.ok) alert("게시물 삭제 실패");
-                else alert("게시물이 삭제되었습니다.");
-            }
-        );
+        
+        fetch(fetchPostsUrl,{method:"DELETE"}).then(response=>{
+            if(!response.ok) alert("게시물 삭제 실패");
+            else alert("게시물이 삭제되었습니다.");
+        });
     }
     dropdown.style.display = "none";
 });
@@ -184,10 +173,8 @@ document.addEventListener("click", function (event) {
 });
 function placeCaretAtEnd(el) {
     el.focus();
-    if (
-        typeof window.getSelection != "undefined" &&
-        typeof document.createRange != "undefined"
-    ) {
+    if (typeof window.getSelection != "undefined"
+        && typeof document.createRange != "undefined") {
         let range = document.createRange();
         range.selectNodeContents(el);
         range.collapse(false);
@@ -202,82 +189,71 @@ function placeCaretAtEnd(el) {
     }
 }
 
-document
-    .getElementById("comment-form")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
+document.getElementById('comment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-        let commentText = document.getElementById("comment-text").value;
-        if (commentText.trim() !== "") {
-            commentText = commentText.replaceAll("\n", "<br>");
-            const commentList = document.getElementById("comment-list");
-            const newComment = document.createElement("div");
-            newComment.classList.add("comment");
-            newComment.innerHTML = `
+    let commentText = document.getElementById('comment-text').value;
+    if (commentText.trim() !== "") {
+        commentText = commentText.replaceAll("\n","<br>");
+        const commentList = document.getElementById('comment-list');
+        const newComment = document.createElement('div');
+        newComment.classList.add('comment');
+        newComment.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                <g clip-path="url(#clip0_2202_235)">
-                    <rect y="0.5" width="24" height="24" rx="12" fill="#CBCCCE" />
-                    <path d="M12.5 15.5C15.1244 15.5 17.25 13.3744 17.25 10.75C17.25 8.12562 15.1244 6 12.5 6C9.87563 6 7.75 8.12562 7.75 10.75C7.75 13.3744 9.87563 15.5 12.5 15.5ZM12.5 17.875C9.32937 17.875 3 19.4662 3 22.625V23.8125C3 24.4656 3.53438 25 4.1875 25H20.8125C21.4656 25 22 24.4656 22 23.8125V22.625C22 19.4662 15.6706 17.875 12.5 17.875Z" fill="white" />
-                </g>
-                <defs>
-                    <clipPath id="clip0_2202_235">
-                        <rect y="0.5" width="24" height="24" rx="12" fill="white" />
-                    </clipPath>
-                </defs>
-            </svg>
+                            <g clip-path="url(#clip0_2202_235)">
+                                <rect y="0.5" width="24" height="24" rx="12" fill="#CBCCCE" />
+                                <path d="M12.5 15.5C15.1244 15.5 17.25 13.3744 17.25 10.75C17.25 8.12562 15.1244 6 12.5 6C9.87563 6 7.75 8.12562 7.75 10.75C7.75 13.3744 9.87563 15.5 12.5 15.5ZM12.5 17.875C9.32937 17.875 3 19.4662 3 22.625V23.8125C3 24.4656 3.53438 25 4.1875 25H20.8125C21.4656 25 22 24.4656 22 23.8125V22.625C22 19.4662 15.6706 17.875 12.5 17.875Z" fill="white" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_2202_235">
+                                    <rect y="0.5" width="24" height="24" rx="12" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
             <strong class="commentAuthor">MY</strong>
             <p>${commentText}</p>`;
-            commentList.appendChild(newComment);
+        commentList.appendChild(newComment);
 
-            document.getElementById("comment-text").value = "";
+        document.getElementById('comment-text').value = "";
 
-            const textarea = document.getElementById("comment-text");
-            textarea.style.height = "50px";
-            const charCount = document.getElementById("char-count");
-            charCount.style.display = "none";
+        const textarea = document.getElementById('comment-text');
+        textarea.style.height = "50px";
+        const charCount = document.getElementById('char-count');
+        charCount.style.display = 'none';
 
-            fetch(fetchCommentsUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    content: commentText,
-                    author: user.nickname,
-                }),
-                credentials: "include",
-            }).then((response) => {
-                if (!response.ok) alert("댓글 등록에 실패했습니다.");
-            });
-        }
-    });
+        fetch(fetchCommentsUrl,{method:"POST",body:{
+            content:commentText,
+            author:user.nickname,
+            credentials:"include"
+        }}).then(response =>{
+            if(!response.ok) alert("댓글 등록에 실패했습니다.");
+        });
+    }
+});
 
-const commentTextElement = document.getElementById("comment-text");
-const maxChars = 100;
+const commentText = document.getElementById("comment-text");
 const charCount = document.getElementById("char-count");
 const initialHeight = 50;
+const maxChars = 100;
 
 function autoResize() {
-    commentTextElement.style.height = `${initialHeight}px`;
-    commentTextElement.style.height = commentTextElement.scrollHeight + "px";
+    commentText.style.height = `${initialHeight}px`;
+    commentText.style.height = commentText.scrollHeight + "px";
 }
 
 function handleExcessCharacters() {
-    const commentText = commentTextElement.value;
+    const commentText = commentText.value;
 
     if (commentText.length > 100) {
-        commentTextElement.value = commentText.slice(0, 100);
+        commentText.value = commentText.slice(0, 100);
     }
 }
 
 function updateCharCount() {
-    const remaining = 100 - commentTextElement.value.length;
-    const currentHeight = commentTextElement.clientHeight;
+    const remaining = 100 - commentText.value.length;
+    const currentHeight = commentText.clientHeight;
 
-    if (
-        currentHeight > initialHeight ||
-        commentTextElement.value.includes("\n")
-    ) {
+    if (currentHeight > initialHeight || commentText.value.includes("\n")) {
         charCount.textContent = `${remaining}`;
         charCount.style.display = "block";
     } else {
@@ -285,20 +261,20 @@ function updateCharCount() {
     }
 }
 
-commentTextElement.addEventListener("input", autoResize);
-commentTextElement.addEventListener("input", handleExcessCharacters);
-commentTextElement.addEventListener("input", updateCharCount);
-commentTextElement.addEventListener("input", function () {
-    const textLength = commentTextElement.value.length;
+commentText.addEventListener("input", autoResize);
+commentText.addEventListener("input", handleExcessCharacters);
+commentText.addEventListener("input", updateCharCount);
+commentText.addEventListener("input", function () {
+    const textLength = commentText.value.length;
     charCount.textContent = maxChars - textLength;
 
     if (textLength > maxChars) {
-        commentTextElement.classList.add("excess");
+        commentText.classList.add("excess");
     } else {
-        commentTextElement.classList.remove("excess");
+        commentText.classList.remove("excess");
     }
 
-    if (commentTextElement.scrollHeight > initialHeight) {
+    if (commentText.scrollHeight > initialHeight) {
         charCount.style.display = "block";
     } else {
         charCount.style.display = "none";
@@ -308,67 +284,24 @@ commentTextElement.addEventListener("input", function () {
 autoResize();
 updateCharCount();
 
-let heartClicked =
-    localStorage.getItem("heartClicked") === "true" ? true : false;
-if (heartClicked) {
-    heartBtn.classList.add("active");
-}
-document.getElementById("heart-button").addEventListener("click", function () {
-    if (!isLogined) {
-        alert("로그인 후 좋아요를 누를 수 있습니다.");
-        return;
-    }
-
-    const heartCount = document.getElementById("heart-count");
+let heartClicked = false;
+document.getElementById('heart-button').addEventListener('click', function() {
+    const heartCount = document.getElementById('heart-count');
     const currentCount = parseInt(heartCount.textContent);
 
-    // 서버로 좋아요 증가 요청 (PATCH)
-    const updateLike = () => {
-        return fetch(`${hostURL}/api/post/${id}/like`, {
-            method: "PATCH",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    };
-
     if (!heartClicked) {
-        updateLike()
-            .then((response) => {
-                if (response.ok) {
-                    heartCount.textContent = currentCount + 1;
-                    this.classList.add("active");
-                    heartClicked = true;
-                    localStorage.setItem("heartClicked", "true");
-                } else {
-                    console.error("좋아요 증가 실패");
-                }
-            })
-            .catch((error) => {
-                console.error("좋아요 증가 요청 에러:", error);
-            });
+        heartCount.textContent = currentCount + 1;
+        this.classList.add('active');
     } else {
-        updateLike()
-            .then((response) => {
-                if (response.ok) {
-                    heartCount.textContent = currentCount - 1;
-                    this.classList.remove("active");
-                    heartClicked = false;
-                    localStorage.setItem("heartClicked", "false");
-                } else {
-                    console.error("좋아요 감소 실패");
-                }
-            })
-            .catch((error) => {
-                console.error("좋아요 감소 요청 에러:", error);
-            });
+        heartCount.textContent = currentCount - 1;
+        this.classList.remove('active');
     }
+    heartClicked = !heartClicked;
 });
 
 // 이전화면으로 이동
 const previous = document.querySelector("#prvBtn");
 
-previous.addEventListener("click", function () {
+previous.addEventListener('click', function(){
     history.back();
 });
